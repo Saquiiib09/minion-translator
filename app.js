e56c07c727748de519f9957bf13a0b320f2e9629
Saquiiib09 <112475828+Saquiiib09@users.noreply.github.com>
2022-10-19 20:55:02 +0530

    notworking

---
+++ app.js
@@ -0,0 +1,55 @@
+var btnTranslate= document.querySelector("#btn-translate");
+var text= document.querySelector("#txt");
+console.log(text);
+
+btnTranslate .addEventListener  ("click", clickHandler() )
+console.log("addEventHandler");
+function clickHandler( ) {
+      console.log("the button is clicked");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var btnTranslate= document.querySelector("#btn-Translate");
+// btnTranslate.addEventListener("click", function clickEventHandler(){
+//       console.log("the button is clicked");
+// });
